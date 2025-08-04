4a33a46630d1e87fb9f917f24ac06b1777324c23
Jean Castor <jeancastor459@gmail.com>
2025-08-04 18:41:01 -0300

    "Adicionando funcionalidade de busca e preenchimento de cep nos inputs html, Adicionando funcionalidade de login"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
+import {LoginComponent} from './components/login/login.component';
+import {NgModule} from '@angular/core';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
@@ -9,5 +11,12 @@ export const routes: Routes = [
   { path: 'users/create', component: UserFormComponent },
   { path: 'users/edit/:id', component: UserFormComponent },
   { path: 'users/details/:id', component: UserDetailsComponent }, // Nova rota
+  { path: 'login', component: LoginComponent},
   { path: '**', redirectTo: '/users' }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
